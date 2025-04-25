77b015f3cb6c88b1b91776161b2797285a78771c
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-25 14:53:55 +0700

    membuat logo pada halaman

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -12,4 +12,27 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool _obscurePassword = true;
-}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Logo Section
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 30),
+                    child: Image.asset(
+                      './assets/images/logo.jpeg',
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
