32c106bc5203caa72d66bc3f11795d26f7e7edd0
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 13:44:51 +0700

    Menambahkan state DataPiketPage dengan form input tugas, tanggal, dan daftar tugas

--- lib/presentation/data_piket_page.dart
+++ lib/presentation/data_piket_page.dart
@@ -4,3 +4,16 @@ class DataPiketPage extends StatefulWidget {
   final String? name;
 
   const DataPiketPage({super.key, this.name});
+
+  @override
+  State<DataPiketPage> createState() => _DataPiketPageState();
+}
+
+class _DataPiketPageState extends State<DataPiketPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController taskController = TextEditingController();
+  DateTime? selectedDate;
+  List<Map<String, String>> taskList = [];
+
+  String? taskError;
+  String? dateError;
