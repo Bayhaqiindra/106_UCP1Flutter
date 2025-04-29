fcb33f8eb0e2b1d6967a5d11e656639968bd7729
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 15:15:38 +0700

    Menambahkan tampilan detail data yang berhasil disimpan

--- lib/presentation/detail_pendataan.dart
+++ lib/presentation/detail_pendataan.dart
@@ -30,3 +30,68 @@ class DetailPendataanPage extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    initializeDateFormatting('id_ID', null);
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F5),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 40),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green.shade100,
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    child: const Icon(
+                      Icons.check_circle,
+                      size: 80,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    'Data Berhasil Disimpan',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 32),
+                  ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      buildDataRow(
+                        'Tanggal',
+                        DateFormat(
+                          'EEEE, dd MMMM yyyy',
+                          'id_ID',
+                        ).format(tanggal),
+                      ),
+                      const Divider(),
+                      buildDataRow('Jenis Transaksi', jenisTransaksi),
+                      const Divider(),
+                      buildDataRow('Jenis Barang', jenisBarang),
+                      const Divider(),
+                      buildDataRow('Jumlah Barang', jumlahBarang.toString()),
+                      const Divider(),
+                      buildDataRow(
+                        'Harga Satuan',
+                        'Rp. ${hargaSatuan.toString()}',
+                      ),
+                      const Divider(),
+                      buildDataRow(
+                        'Total Harga',
+                        'Rp. ${totalHarga.toString()}',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
