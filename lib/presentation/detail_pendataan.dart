7fce5417eb441962111aa999c06f98c12160f1af
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 15:13:31 +0700

    Menambahkan halaman detail pendataan barang

--- lib/presentation/detail_pendataan.dart
+++ lib/presentation/detail_pendataan.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class DetailPendataanPage extends StatelessWidget {
+  final DateTime tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+  final int totalHarga;
+
+  const DetailPendataanPage({
+    Key? key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+    required this.totalHarga,
+  }) : super(key: key);
