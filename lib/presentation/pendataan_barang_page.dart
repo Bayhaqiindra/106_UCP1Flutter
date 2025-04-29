82fed4eead696c3419eefa28be159a7638a0cfd0
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:56:27 +0700

    Menambahkan opsi prefix icon dan style pada InputDecoration.

--- lib/presentation/pendataan_barang_page.dart
+++ lib/presentation/pendataan_barang_page.dart
@@ -21,3 +21,42 @@ class _BarangPageState extends State<BarangPage> {
     'Tenda': 2500000,
     'Sepatu': 1000000,
   };
+
+  InputDecoration buildInputDecoration(
+  String hintText, {
+  Widget? prefixIcon,
+  Widget? prefix,
+  String? prefixText,
+}) {
+  return InputDecoration(
+    hintText: hintText,
+    prefixIcon: prefixIcon,
+    prefix: prefix,
+    prefixText: prefixText,
+    prefixStyle: const TextStyle(color: Colors.black),
+    hintStyle: const TextStyle(color: Colors.black54),
+    filled: true,
+    fillColor: Colors.white,
+    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.black26),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.black26),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.black54),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.red),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.red),
+    ),
+  );
+}
