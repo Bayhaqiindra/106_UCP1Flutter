5d0da44ebc845f71073f7049648e5b97481b1815
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:54:53 +0700

    Menambahkan mapping harga barang berdasarkan jenis barang.

--- lib/presentation/pendataan_barang_page.dart
+++ lib/presentation/pendataan_barang_page.dart
@@ -15,3 +15,9 @@ class _BarangPageState extends State<BarangPage> {
   String? jenisBarang;
   final jumlahBarangController = TextEditingController();
   final hargaSatuanController = TextEditingController();
+  final Map<String, int> hargaBarang = {
+    'Carrier': 1500000,
+    'Sleeping Bag': 500000,
+    'Tenda': 2500000,
+    'Sepatu': 1000000,
+  };
