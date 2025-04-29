79a2dce4020ea5fea79cd3ca663da20ef7680dc8
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:50:15 +0700

    Menambahkan form untuk input barang dengan kolom tanggal, jenis transaksi, jenis barang, jumlah, dan harga satuan.

--- lib/presentation/pendataan_barang_page.dart
+++ lib/presentation/pendataan_barang_page.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class BarangPage extends StatefulWidget {
+  const BarangPage({Key? key}) : super(key: key);
+
+  @override
+  State<BarangPage> createState() => _BarangPageState();
+}
+
+class _BarangPageState extends State<BarangPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  DateTime? selectedDate;
+  String? jenisTransaksi;
+  String? jenisBarang;
+  final jumlahBarangController = TextEditingController();
+  final hargaSatuanController = TextEditingController();
