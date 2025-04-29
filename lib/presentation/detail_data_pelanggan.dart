21730908e91efde68f09af2acfe9f9a4daac6d43
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:36:10 +0700

    Menambahkan konstruktor dan properti untuk DetailPelangganPage

--- lib/presentation/detail_data_pelanggan.dart
+++ lib/presentation/detail_data_pelanggan.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class DetailPelangganPage extends StatelessWidget {
+  final String nama;
+  final String email;
+  final String hp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+
+  const DetailPelangganPage({
+    Key? key,
+    required this.nama,
+    required this.email,
+    required this.hp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos,
+  }) : super(key: key);
