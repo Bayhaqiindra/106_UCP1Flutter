eed6ec1798b92c233b643902a89b6ed9d4b70c53
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 13:04:22 +0700

    Menambahkan tampilan avatar dan nama pengguna di AppBar

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -5,3 +5,49 @@ class HomePage extends StatelessWidget {
   final String? email;
 
   const HomePage({super.key, this.name, this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    String displayText = name ?? email ?? 'User'; // fallback kalau null
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+        elevation: 0,
+        titleSpacing: 0,
+        toolbarHeight: 70,
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.white,
+                child: ClipOval(
+                  child: Image.asset('./assets/images/avatar.png'),
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Selamat Datang',
+                  style: TextStyle(fontSize: 12, color: Colors.white70),
+                ),
+                Text(
+                  displayText,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
