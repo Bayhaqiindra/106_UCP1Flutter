1b678910ecd49dd29c4d4640b01ea8c9a2c3bcc2
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:07:32 +0700

    Menambahkan halaman DataPelangganPage dengan StatefulWidget

--- lib/presentation/data_pelanggan_page.dart
+++ lib/presentation/data_pelanggan_page.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class DataPelangganPage extends StatefulWidget {
+  const DataPelangganPage({Key? key}) : super(key: key);
+
+  @override
+  _DataPelangganPageState createState() => _DataPelangganPageState();
+}
