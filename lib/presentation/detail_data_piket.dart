19c5db8f0045d1673a5bfbf4d9a3edd3aa22aaef
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:01:52 +0700

    Membuat halaman TaskDetailPage

--- lib/presentation/detail_data_piket.dart
+++ lib/presentation/detail_data_piket.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class TaskDetailPage extends StatelessWidget {
+  final String taskTitle;
+  final String taskDate; // contoh "29/4/2025"
+  final String userName;
+
+  const TaskDetailPage({
+    super.key,
+    required this.taskTitle,
+    required this.taskDate,
+    required this.userName,
+  });
