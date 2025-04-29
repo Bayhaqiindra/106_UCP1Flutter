cc3cbf7d9cc09d21d1deedf04dce3504d5f14bd4
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 12:47:18 +0700

    buat form register dengan validasi input

--- lib/presentation/register.page.dart
+++ lib/presentation/register.page.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
