e96968b7c8cc9b93c3041f50b3265203ab0dc4cd
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:42:14 +0700

    Menambahkan kustomisasi InputDecoration

--- lib/presentation/detail_data_pelanggan.dart
+++ lib/presentation/detail_data_pelanggan.dart
@@ -17,3 +17,25 @@ class DetailPelangganPage extends StatelessWidget {
     required this.provinsi,
     required this.kodePos,
   }) : super(key: key);
+
+  InputDecoration buildInputDecoration(String hintText) {
+    return InputDecoration(
+      hintText: hintText,
+      hintStyle: const TextStyle(color: Colors.black87),
+      filled: true,
+      fillColor: Colors.white,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(color: Colors.black26),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(color: Colors.black26),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(color: Colors.black54),
+      ),
+    );
+  }
