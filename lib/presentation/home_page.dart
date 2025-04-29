5c915b06c6a497729b320b42ba58066bea8dfce4
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 13:03:17 +0700

    Menambahkan parameter name dan email pada konstruktor HomePage

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String? name;
+  final String? email;
+
+  const HomePage({super.key, this.name, this.email});
