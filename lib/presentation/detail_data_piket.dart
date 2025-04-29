b53113e54226e12417fd50a67bca7b0add5ea1e9
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-04-29 14:03:26 +0700

    Menambahkan AppBar

--- lib/presentation/detail_data_piket.dart
+++ lib/presentation/detail_data_piket.dart
@@ -21,3 +21,27 @@ class TaskDetailPage extends StatelessWidget {
       'EEEE, dd MMMM yyyy',
       'id_ID',
     ).format(parsedDate);
+
+return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Detail $taskTitle',
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
